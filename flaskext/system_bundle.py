82d1d3128a123b813385796d2464a516ca9b119f
Yohan Graterol <yohangraterol92@gmail.com>
2013-07-26 23:27:28 -0430

    Add loader blueprints

--- flaskext/system_bundle.py
+++ flaskext/system_bundle.py
@@ -9,44 +9,50 @@
     :copyright: (c) 2013 by yograterol.
     :license: BSD, see LICENSE for more details.
 """
-from yapsy.IPlugin import IPlugin
-from yapsy.PluginManager import PluginManager
-from flask import (Flask, Blueprint)
-
-
-class Bundle(Blueprint, IPlugin):
-
-    def __init__(self, *args, **kwargs):
-        if not 'static_folder' in kwargs.keys():
-            kwargs['static_folder'] = 'static'
-        if not 'template_folder' in kwargs.keys():
-            kwargs['template_folder'] = 'template'
-        super(Bundle, self).__init__(*args, **kwargs)
-
-    def register_blueprint(self, *args, **kwargs):
-        super.register(*args, **kwargs)
+from distutils.sysconfig import get_python_lib
+from os.path import join as joinpath
+from os import (listdir, exists, isdir, splitext)
+from imp import (find_module, load_module)
+from flask import Flask
 
 
 class SystemBundle(object):
 
-    __slots__ = ['app', 'debug', 'manager', 'name_folder_bundle']
+    __slots__ = ['app', 'debug', 'bundle_namespace', 'test']
 
     def __init__(self, name, debug=False, config_object=None,
-                 name_folder_bundle='bundle'):
+                 bundle_namespace='bundle', test=False):
         self.app = Flask(name)
         self.debug = debug
         self.app.config.from_object(config_object)
-        self.name_folder_bundle = name_folder_bundle
+        if isdir(bundle_namespace):
+            self.bundle_namespace = bundle_namespace
+        else:
+            self.bundle_namespace = joinpath(get_python_lib(), bundle_namespace)
+        self.test = test
 
-    def create_manager(self):
-        self.manager = PluginManager()
-        self.manager.setPluginPlaces([self.name_folder_bundle,])
-        self.manager.collectPlugins()
+    def load_module(self):
+        init = '__init__.py'
+        bundles_list = listdir(self.bundle_namespace)
+        bundles = dict()
 
-    def register_bundle(self):
-        for bundle in self.manager.getAllPlugins():
-            bundle.plugin_object.register_blueprint(self.app)
+        for bundle_name in bundles_list:
+            f, filename, descr = find_module(bundle_name, [self.bundle_namespace])
+            if isdir(joinpath(self.bundle_namespace, bundle_name)) and \
+                exists(joinpath(self.bundle_namespace, bundle_name, init)):
+                bundles[bundle_name] = load_module(bundle_name, f, filename, descr)
+            name, extension = splitext(bundle_name)
+            if extension == '.py' and not name == init:
+                bundles[bundle_name] = load_module(bundle_name, f, filename, descr)
+        self.register_blueprint(bundles_list)
+
+    def register_blueprint(self, bundles_list):
+        for bundle in bundles_list.iteritems():
+            self.app.register_blueprint(getattr(bundle, 'bundle'))
 
     def load_app(self):
+        if self.test:
+            self.app.config['TESTING'] = True
+            return self.app.test_client()
         self.app.run(debug=self.debug)
 
